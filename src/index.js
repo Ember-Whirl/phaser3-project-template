eb51f32d691cdf90374c43dee556f183e7ecb780
DESKTOP-Sly\Sly <sly_emenike@hotmail.com>
2021-02-05 21:53:52 +0100

    Scale change

--- src/index.js
+++ src/index.js
@@ -5,10 +5,15 @@ import "regenerator-runtime/runtime.js";
 
 const config = {
     type: Phaser.AUTO,
-    parent: 'phaser-example',
-    width: 800,
-    height: 600,
-    scene: [BootScene, MainScene]
+    scale: {
+      mode: Phaser.Scale.FIT,
+      autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+    width: 1280,
+    height: 720,
+    OrientationType: 'LANDSCAPE',
+    scene: [BootScene, MainScene],
+    parent: 'content'
 };
 
 const game = new Phaser.Game(config);
