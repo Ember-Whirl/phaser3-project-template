c748160a0ae584250fad1ae425e699fd20613083
ThijsAdema <thijsadema@hotmail.com>
2024-04-18 20:27:50 +0200

    Changed values back

--- src/scripts/managers/standard-managers/warriorSpawner.js
+++ src/scripts/managers/standard-managers/warriorSpawner.js
@@ -14,7 +14,7 @@ export default class WarriorSpawner extends Singleton {
 
         this.spawnedWarriors = []
         this.warriorCount = 0
-        this.warriorSpawnTime = 0.0002
+        this.warriorSpawnTime = 2
         this.warriorSpawnTimeCounter = 0
         this.nextWarriorID = 0
 
@@ -22,7 +22,7 @@ export default class WarriorSpawner extends Singleton {
 
         this.maxLevel = this.warriorTypesData.warriorTypes.length
         this.spawningLevel = 1
-        this.maxAmountOfWarriors = 100
+        this.maxAmountOfWarriors = 8
 
         this.gameInProgress = true
 
