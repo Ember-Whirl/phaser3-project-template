b1233eda737dcd99062a57244056c74f3b921d2d
Sly <sly@emberwhirl.com>
2026-01-17 17:41:31 +0100

    Updated to webpack 5

--- webpack/base.js
+++ webpack/base.js
@@ -4,8 +4,13 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
+  entry: "./src/index.js",
   mode: "development",
   devtool: "eval-source-map",
+  output: {
+    path: path.resolve(__dirname, "../dist"),
+    clean: true
+  },
   module: {
     rules: [
       {
@@ -17,17 +22,17 @@ module.exports = {
       },
       {
         test: [/\.vert$/, /\.frag$/],
-        use: "raw-loader"
+        type: "asset/source"
       },
       {
-        test: /\.(gif|png|jpe?g|svg|xml)$/i,
-        use: "file-loader"
+        test: /\.(gif|png|jpe?g|svg|xml|json|atlas)$/i,
+        type: "asset/resource"
       }
     ]
   },
   plugins: [
     new CleanWebpackPlugin({
-      root: path.resolve(__dirname, "../")
+      cleanOnceBeforeBuildPatterns: ["**/*", "!index.html"]
     }),
     new webpack.DefinePlugin({
       __platform__: JSON.stringify("Web"),
@@ -38,5 +43,15 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "./index.html"
     })
-  ]
+  ],
+  devServer: {
+    open: true,
+    port: 8080,
+    static: [
+      {
+        directory: path.join(__dirname, ".."),
+        publicPath: "/"
+      }
+    ]
+  }
 };
--- webpack/prod.js
+++ webpack/prod.js
@@ -1,11 +1,14 @@
-const merge = require("webpack-merge");
+const { merge } = require("webpack-merge");
 const path = require("path");
+const webpack = require("webpack");
 const base = require("./base");
 const TerserPlugin = require("terser-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = merge(base, {
   mode: "production",
   output: {
+    path: path.resolve(__dirname, "../dist"),
     filename: "bundle.min.js"
   },
   devtool: false,
@@ -17,10 +20,11 @@ module.exports = merge(base, {
     minimizer: [
       new TerserPlugin({
         terserOptions: {
-          output: {
+          format: {
             comments: false
           }
-        }
+        },
+        extractComments: false
       })
     ]
   },
@@ -31,5 +35,13 @@ module.exports = merge(base, {
       CANVAS_RENDERER: JSON.stringify(true),
       WEBGL_RENDERER: JSON.stringify(true)
     }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "../src/assets"),
+          to: path.resolve(__dirname, "../dist/src/assets")
+        }
+      ]
+    })
   ]
 });
